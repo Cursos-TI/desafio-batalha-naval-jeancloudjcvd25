a44198f1e001712d4e03c921e16f5bdbef805b6e
jeancloudjcvd25 <vandamme-pereira@hotmail.com>
2025-08-19 00:02:24 +0000

    Primeria-Alteracao

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -35,6 +35,25 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
+    char linha[] = {'A','B','C','D','E','F','G','H','I','J'};
+    int tabuleiro[10][10] = {0};  // inicializa tudo com 0
 
+    printf("\n...JOGO DE BATALHA NAVAL...\n\n");
+
+    // Imprime cabeçalho com letras
+    printf("   ");
+    for (int j = 0; j < 10; j++) {
+        printf(" %c ", linha[j]);
+    }
+    printf("\n");
+
+    // Imprime as linhas com números
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i + 1);  // numeração das linhas (1 a 10)
+        for (int j = 0; j < 10; j++) {
+            printf(" 0 "); // símbolo do mar
+        }
+        printf("\n");
+    }
     return 0;
 }
